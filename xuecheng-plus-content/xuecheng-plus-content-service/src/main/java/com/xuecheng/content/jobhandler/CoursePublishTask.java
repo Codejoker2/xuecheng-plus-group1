7eff8e7ba3f199f596045f6443c95f588dd3db5c
codejoker2 <weichuanzeng@gmail.com>
2024-05-01 17:51:27 +0800

    课程发布调度步骤框架

--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/jobhandler/CoursePublishTask.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/jobhandler/CoursePublishTask.java
@@ -1,8 +1,15 @@
 package com.xuecheng.content.jobhandler;
 
+import com.xuecheng.messagesdk.model.po.MqMessage;
+import com.xuecheng.messagesdk.service.MessageProcessAbstract;
+import com.xuecheng.messagesdk.service.MqMessageService;
+import com.xxl.job.core.context.XxlJobHelper;
 import com.xxl.job.core.handler.annotation.XxlJob;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author zengweichuan
@@ -11,10 +18,73 @@ import org.springframework.stereotype.Component;
  */
 @Slf4j
 @Component
-public class CoursePublishTask {
+public class CoursePublishTask extends MessageProcessAbstract {
+
+    @Resource
+    private MqMessageService mqMessageService;
     //任务调度入口
     @XxlJob("CoursePublishJobHandler")
     public void coursePublishJobHandler(){
-        System.out.println("牛逼");
+        // 分片参数
+        int shardIndex = XxlJobHelper.getShardIndex();
+        int shardTotal = XxlJobHelper.getShardTotal();
+        log.debug("shardIndex="+shardIndex+",shardTotal="+shardTotal);
+        //参数:分片序号、分片总数、消息类型、一次最多取到的任务数量、一次任务调度执行的超时时间
+        process(shardIndex,shardTotal,"course_publish",30,60);
+
+    }
+
+    @Override
+    public boolean execute(MqMessage mqMessage) {
+
+        //获取消息相关的业务信息
+        String courseId = mqMessage.getBusinessKey1();
+        //课程静态化
+        generateCourseHtml(mqMessage,courseId);
+        //课程索引
+        saveCourseIndex(mqMessage,courseId);
+        //课程缓存
+        saveCourseCache(mqMessage,courseId);
+        return true;
+    }
+
+    private void saveCourseCache(MqMessage mqMessage, String courseId) {
+
+        log.debug("开始进行课程静态化,课程id:{}",courseId);
+        //消息id
+        Long id = mqMessage.getId();
+        //消息处理的service
+        MqMessageService mqMessageService = this.getMqMessageService();
+        //消息幂等性处理
+        int stageOne = mqMessageService.getStageOne(id);
+        if(stageOne >0){
+            log.debug("课程静态化已处理直接返回，课程id:{}",courseId);
+            return ;
+        }
+        try {
+            TimeUnit.SECONDS.sleep(10);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        //保存第一阶段状态
+        mqMessageService.completedStageOne(id);
+    }
+
+    private void generateCourseHtml(MqMessage mqMessage, String courseId) {
+        log.debug("将课程信息缓存至redis,课程id:{}",courseId);
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void saveCourseIndex(MqMessage mqMessage, String courseId) {
+        log.debug("保存课程索引信息,课程id:{}",courseId);
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
