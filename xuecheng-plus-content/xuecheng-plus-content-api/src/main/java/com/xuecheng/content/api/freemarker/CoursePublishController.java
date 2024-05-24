4b7cc664c723ca91a431ed242565030caad50ff4
zengweichuan <3076368691@qq.com>
2024-05-24 10:30:17 +0800

    解决以来冲突问题

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/freemarker/CoursePublishController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/freemarker/CoursePublishController.java
@@ -5,6 +5,8 @@ import com.xuecheng.content.service.CoursePublishService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
@@ -27,5 +29,17 @@ public class CoursePublishController {
         modelAndView.addObject("model",coursePreviewDto);
         modelAndView.setViewName("course_template");
         return modelAndView;
+    }
+
+    /**
+     * 课程发布
+     * @param courseId
+     */
+    @ResponseBody
+    @PostMapping("/coursepublish/{courseId}")
+    public void coursePublish(@PathVariable Long courseId){
+        //机构id，由于认证系统没有上线暂时硬编码
+        Long companyId = 1232141425L;
+        coursePublishService.coursePublish(companyId,courseId);
     }
 }
