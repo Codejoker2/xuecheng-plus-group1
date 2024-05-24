330e6e86b29230c9c40e0fa81c399323f61a95c8
zengweichuan <3076368691@qq.com>
2024-05-24 09:26:29 +0800

    添加熔断降级功能，并进行测试

--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/feignclient/MediaServiceClient.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/feignclient/MediaServiceClient.java
@@ -3,10 +3,7 @@ package com.xuecheng.content.feignclient;
 import com.xuecheng.content.config.MultipartSupportConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 /**
@@ -15,7 +12,9 @@ import org.springframework.web.multipart.MultipartFile;
  * @date 2022/9/20 20:29
  * @version 1.0
  */
-@FeignClient(value = "media-api",configuration = MultipartSupportConfig.class)
+@FeignClient(value = "media-api",
+        configuration = MultipartSupportConfig.class,
+        fallbackFactory = MediaServiceClientFallbackFactory.class)
 public interface MediaServiceClient {
 
     /*
